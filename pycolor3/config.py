7514c76a9e2585cf7db436dd131cd6f5e596f5f7
Josh Farwell <jfarwell@newrelic.com>
2018-05-01 14:12:42 -0700

    Implemented iColor3 serial protocol class
    Signed-off-by: Josh Farwell <jfarwell@newrelic.com>

--- pycolor3/config.py
+++ pycolor3/config.py
@@ -3,3 +3,4 @@ import os
 class BaseConfig:
 
     SERIAL_DEVICE = os.environ.get('PYCOLOR3_SERIAL_DEVICE')
+    SERIAL_TIMEOUT = os.environ.get('PYCOLOR3_SERIAL_TIMEOUT', 5)
--- pycolor3/serial_api.py
+++ pycolor3/serial_api.py
@@ -1,11 +1,52 @@
 import serial
+from functools import wraps
+
+
+class IColor3SerialError(Exception):
+    pass
 
 
 class SerialAPI:
     def __init__(self, config):
-        self.device = config['SERIAL_DEVICE']
-        self.conn = None
+        self.conn = serial.Serial()
 
-    def connect(self):
-        self.conn = serial.Serial(self.device)
-        return self.conn
+        # Settings for iColor3
+        self.conn.baudrate = 9600
+        self.conn.bytesize = 8
+        self.conn.parity = 'N'
+        self.conn.stopbits = 1
+
+        # Configured via Flask app
+        self.conn.timeout = config['SERIAL_TIMEOUT']
+        self.conn.port = config['SERIAL_DEVICE']
+
+    def __enter__(self):
+        self.conn.open()
+        return self
+
+    def __exit__(self, *args):
+        self.conn.close()
+
+    def send_command(self, prefix, input):
+        if not self.conn.is_open:
+            raise IColor3SerialError
+
+        command = prefix + input
+        self.conn.write(command.encode())
+
+        response = self.conn.read(5)
+        if not response:
+            return False
+        response = response.decode()
+        if response.replace('Y', 'X') != command:
+            raise IColor3SerialError(response)
+        return True
+
+    def play_show(self, show_number):
+        return self.send_command("X04", hex(show_number)[2:].zfill(2))
+
+    def turn_off(self):
+        return self.send_command("X01", "00")
+
+    def set_brightness(self, brightness):
+        return self.send_command("X02", hex(brightness)[2:].zfill(2))
